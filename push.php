d178549cda367bdd9d1960343cb8d4983dc59a66
uj-on-kaist <jung-1028@kaist.ac.kr>
2015-03-12 01:02:43 +0900

    1st upload

--- push.php
+++ push.php
@@ -1,3 +1,57 @@
 <?php
 
+require  'medoo.min.php';
+require  'settings.php';
+
+$headers = array(
+'Content-Type:application/json',
+'Authorization:key=AIzaSyDktZ4hsSv2SbwHPUhy22CJh2jcn81qdgw'
+);
+
+$database = new medoo([
+	// required
+	'database_type' => 'mysql',
+	'database_name' => $DB_NAME,
+	'server' => $DB_HOST,
+	'username' => $DB_USERNAME,
+	'password' => $DB_PASSWORD,
+	'charset' => $DB_CHARSET,
+ 	'port' => 3306
+]);
+ 
+
+#Get Android Push Messages
+$filter = ["AND" => ["processed" => false,"device_type" => 1], "LIMIT" => 100];
+$datas = $database->select("controller_pushnotifications", "*",$filter);
+
+
+foreach($datas as $msg){
+	$arr   = array();
+	$arr['data'] = array();
+	$arr['data']['contents'] = $msg["contents"];
+	$arr['registration_ids'] = array();
+	$arr['registration_ids'][0] = $msg["device_id"];
+	
+	try{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,    'https://android.googleapis.com/gcm/send');
+		curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
+		curl_setopt($ch, CURLOPT_POST,    true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arr));
+		$response_json = curl_exec($ch);
+		$response = json_decode($response_json, true);
+		if($response["success"] == 1){
+			//success
+			$database->update("controller_pushnotifications",["processed" => true] ,["id" => $msg["id"]]);
+		}else{
+			//fail
+		}
+		curl_close($ch);
+	} catch (Exception $e) {
+    	//echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+}
+
 ?>
---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$DB_NAME = 'tyrant';
+$DB_HOST = "127.0.0.1";
+$DB_USERNAME = "root";
+$DB_PASSWORD = "";
+$DB_CHARSET = "utf8";
+
+?>
